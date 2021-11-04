0dd6c421b0b54c65382d9374511dd22331c46226
Thomas <tg.59290@gmail.com>
2021-11-04 14:39:02 +0100

    add basic template for the todolist in react

--- frontend/framework/exercicesreact/src/exercices/Todolist.js
+++ frontend/framework/exercicesreact/src/exercices/Todolist.js
@@ -1,8 +1,17 @@
 function Todolist() {
     return(
-        <div>
+        <section className="my-4">
             <h2>Todolist :</h2>
-        </div>
+            <div className="row">
+                <div className="col-6">
+                    <input className="form-control w-75" type="text" value="A faire"></input>
+                    <button className="btn btn-secondary my-3">Ajouter</button>
+                </div>
+                <div className="col-6">
+                    <h3>Liste de tâches à réaliser</h3>
+                </div>
+            </div>
+        </section>
     );
 }
 
