b5be57e02502b9bbd38a50fe222c43812b1ec0e5
Thomas <tg.59290@gmail.com>
2021-11-04 14:30:14 +0100

    add comments and class exemple for light exercice

--- frontend/framework/exercicesreact/src/exercices/Presentation.js
+++ frontend/framework/exercicesreact/src/exercices/Presentation.js
@@ -1,6 +1,10 @@
+// Composant de l'exercice 1
+// Exemple de composant fonctionnel qui utilise les props (données figées)
 function Presentation(props) {
+    // On génère les li de la liste sur la base de l'objet passé en props (voir app.js)
+    // La loop react standard et le map
     const userList = Object.keys(props.user).map((key) => <li className="list-group-item" key={key}>{key + " : " + props.user[key]}</li>);
-    
+    // Affichage de la liste
     return(
         <section className="my-4">
             <h2>Présentation</h2>
